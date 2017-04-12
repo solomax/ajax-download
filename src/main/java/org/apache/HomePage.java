ed5c8f898318da9933369c4bc5ecb5f12fb046c1
Maxim Solodovnik <solomax666@gmail.com>
2017-04-12 18:10:43 +0700

    Quickstart is updated to show various types of downloads

--- src/main/java/org/apache/HomePage.java
+++ src/main/java/org/apache/HomePage.java
@@ -5,7 +5,9 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.time.LocalDateTime;
 import java.util.Locale;
 import java.util.zip.ZipEntry;
@@ -30,7 +32,7 @@ import com.googlecode.wicket.kendo.ui.form.datetime.local.DateTimePicker;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
-	private File zipFile;
+	private File dwnldFile;
 
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
@@ -38,12 +40,12 @@ public class HomePage extends WebPage {
 
 		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
 
-		final AjaxDownload download = new AjaxDownload(new IResource() {
+		final AjaxDownload downloadBlob = new AjaxDownload(new IResource() {
 			private static final long serialVersionUID = 1L;
 
 			@Override
 			public void respond(Attributes attributes) {
-				new FileSystemResource(zipFile.toPath()) {
+				new FileSystemResource(dwnldFile.toPath()) {
 					private static final long serialVersionUID = 1L;
 
 					@Override
@@ -55,31 +57,84 @@ public class HomePage extends WebPage {
 				}.respond(attributes);
 			}
 		});
+		final AjaxDownload downloadFrame = new AjaxDownload(new IResource() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void respond(Attributes attributes) {
+				new FileSystemResource(dwnldFile.toPath()) {
+					private static final long serialVersionUID = 1L;
+
+					@Override
+					protected ResourceResponse createResourceResponse(Path path) {
+						ResourceResponse response = super.createResourceResponse(path);
+						response.setFileName("" + path.getFileName());
+						return response;
+					}
+				}.respond(attributes);
+			}
+		}).setLocation(AjaxDownload.Location.IFrame);
 		final FeedbackPanel feedback = new FeedbackPanel("feedback");
-		add(download);
+		add(downloadBlob, downloadFrame);
 		add(new Form<Void>("form").add(
 				feedback.setOutputMarkupId(true)
-				, new AjaxButton("download") {
+				, new AjaxButton("download-zip-blob") {
 					private static final long serialVersionUID = 1L;
 
 					@Override
 					protected void onSubmit(AjaxRequestTarget target) {
-						StringBuilder sb = new StringBuilder("Test String");
-						try {
-							zipFile = File.createTempFile("wicketTest", ".zip");
-							try (ZipOutputStream _zip = new ZipOutputStream(new FileOutputStream(zipFile))) {
-								ZipEntry e = new ZipEntry("mytext.txt");
-								_zip.putNextEntry(e);
+						createZip();
+						info("ZIP Blob, filename: " + dwnldFile.getName());
+						target.add(feedback);
+						downloadBlob.initiate(target);
+					}
 
-								byte[] data = sb.toString().getBytes(UTF_8);
-								_zip.write(data, 0, data.length);
-								_zip.closeEntry();
-							}
-						} catch (IOException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-						download.initiate(target);
+					@Override
+					protected void onError(AjaxRequestTarget target) {
+						target.add(feedback);
+					}
+				}
+				, new AjaxButton("download-text-blob") {
+					private static final long serialVersionUID = 1L;
+
+					@Override
+					protected void onSubmit(AjaxRequestTarget target) {
+						createText();
+						info("Text Blob, filename: " + dwnldFile.getName());
+						target.add(feedback);
+						downloadBlob.initiate(target);
+					}
+
+					@Override
+					protected void onError(AjaxRequestTarget target) {
+						target.add(feedback);
+					}
+				}
+				, new AjaxButton("download-zip-frame") {
+					private static final long serialVersionUID = 1L;
+
+					@Override
+					protected void onSubmit(AjaxRequestTarget target) {
+						createZip();
+						info("ZIP iFrame, filename: " + dwnldFile.getName());
+						target.add(feedback);
+						downloadFrame.initiate(target);
+					}
+
+					@Override
+					protected void onError(AjaxRequestTarget target) {
+						target.add(feedback);
+					}
+				}
+				, new AjaxButton("download-text-frame") {
+					private static final long serialVersionUID = 1L;
+
+					@Override
+					protected void onSubmit(AjaxRequestTarget target) {
+						createText();
+						info("Text iFrame, filename: " + dwnldFile.getName());
+						target.add(feedback);
+						downloadFrame.initiate(target);
 					}
 
 					@Override
@@ -89,6 +144,35 @@ public class HomePage extends WebPage {
 				}
 				, new DateTimePicker("dateTime", Model.of(LocalDateTime.now()), WebSession.get().getLocale()).setLabel(Model.of("Test DateTime"))
 			));
+	}
+
+	private void createText() {
+		StringBuilder sb = new StringBuilder("Test String ");
+		try {
+			dwnldFile = File.createTempFile("wicketTest", ".html");
+			Files.write(dwnldFile.toPath(), sb.append(dwnldFile.getCanonicalPath()).toString().getBytes(UTF_8), StandardOpenOption.WRITE);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+
+	private void createZip() {
+		StringBuilder sb = new StringBuilder("Test String ");
+		try {
+			dwnldFile = File.createTempFile("wicketTest", ".zip");
+			try (ZipOutputStream _zip = new ZipOutputStream(new FileOutputStream(dwnldFile))) {
+				ZipEntry e = new ZipEntry("mytext.txt");
+				_zip.putNextEntry(e);
+
+				byte[] data = sb.append(dwnldFile.getCanonicalPath()).toString().getBytes(UTF_8);
+				_zip.write(data, 0, data.length);
+				_zip.closeEntry();
+			}
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 	}
 
 	@Override
